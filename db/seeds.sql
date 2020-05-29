2090d8d2703e1ad0dc79e3e88f19b33914ae8635
Milena <milena.mihneva@gmail.com>
2020-05-29 11:58:16 -0600

    untracked node_modules

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ("hamburger", false), ("cheeseburger", false), ("tofuburger", false);
+VALUES ("Classic Cheeseburger", false), ("Build you own", false), ("Vegan burger", false);
